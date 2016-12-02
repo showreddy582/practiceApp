45e44e0c4ddda5bbefc13b8cfdd7dfdec78674d0
bhiapp4 <bhiapp4@gmail.com>
2016-12-02 11:50:12 -0600

    Added space

--- src/main/java/com/courseapp/config/SwaggerConfig.java
+++ src/main/java/com/courseapp/config/SwaggerConfig.java
@@ -15,6 +15,7 @@ public class SwaggerConfig {
 
 	@Bean
     public Docket api() { 
+		
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
